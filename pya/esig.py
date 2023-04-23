73edffc551275958cceb74fcce9c7de59d5f5c30
Bjoern Buschkaemper <bjoern.buschkaemper@gmail.com>
2023-04-23 22:45:14 +0200

    Added vibrato rate.

--- pya/esig.py
+++ pya/esig.py
@@ -12,6 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import librosa
 from pya.asig import Asig
+import scipy.signal
 
 
 class Esig:
@@ -22,7 +23,11 @@ class Esig:
     """
 
     def __init__(
-        self, asig: Asig, algorithm: str = "yaapt", max_vibrato_extent: int = 40
+        self,
+        asig: Asig,
+        algorithm: str = "yaapt",
+        max_vibrato_extent: float = 40,
+        max_vibrato_rate: float = 10.0,
     ) -> None:
         """Creates a new editable audio signal from an existing audio signal.
 
@@ -33,15 +38,19 @@ class Esig:
         algorithm : str
             The algorithm to be used to guess the pitch of the audio signal.
             Possible values are: 'yaapt'
-        max_vibrato_extent : int
+        max_vibrato_extent : float
             The maximum difference between the average pitch of a note to each pitch in the note,
             in cents (100 cents = 1 semitone).
             Voice vibrato is usually below 100 cents.
+        max_vibrato_rate : float
+            The maximum vibrato rate in Hz.
+            Voice vibrato is usually between 5 and 8 Hz.
         """
 
         self.asig = asig
         self.algorithm = algorithm
         self.max_vibrato_extent = max_vibrato_extent
+        self.max_vibrato_rate = max_vibrato_rate
         self.edits = []
 
         # Guess the pitch of the audio signal
@@ -51,9 +60,11 @@ class Esig:
 
             # Apply YAAPT
             pitch_guess = pYAAPT.yaapt(
-                signal, frame_length=30, tda_frame_length=40, f0_min=75, f0_max=600
+                signal, frame_length=30, tda_frame_length=40, f0_min=60, f0_max=600
             )
             self.pitch = pitch_guess.samp_values
+            length = signal.size / signal.fs  # Length of the signal in seconds
+            self.pitch_sr = len(self.pitch) / length  # Pitch sampling rate
         else:
             raise ValueError("Invalid algorithm")
 
@@ -77,11 +88,19 @@ class Esig:
         start = 0
         end = 0
         for i, current_pitch in enumerate(self.pitch):
-            # If we have no pitches yet but a current pitch, start a new note
+            # If the pitch is 0, end the current note if there is one
+            if current_pitch == 0:
+                if pitches is not None:
+                    end = i
+                    ranges.append((start, end))
+                    pitches = None
+
+                continue
+
+            # If we have no pitches, start a new note
             if pitches is None:
-                if current_pitch > 0:
-                    pitches = [current_pitch]
-                    start = i
+                pitches = [current_pitch]
+                start = i
             else:
                 new_avg = np.mean(pitches + [current_pitch])
                 new_avg_midi = librosa.hz_to_midi(new_avg)
@@ -92,16 +111,38 @@ class Esig:
                     self.max_vibrato_extent / 100
                 )  # Max deviation in Hz
 
-                # If adding the current pitch to the note would make any pitch too far away,
-                # end the current note and start a new one
+                end_note = False
+
+                # If adding the current pitch to the note would make any pitch difference
+                # to the mean above the max deviation, end the current note and start a new one
                 if any(abs(pitch - new_avg) > max_freq_deviation for pitch in pitches):
+                    end_note = True
+
+                # If the vibrato rate of the current note is above the max, end the current note
+                # and start a new one, e.g. consider it as a seperate note instead of a vibrato
+                peaks = scipy.signal.find_peaks(self.pitch, prominence=0.1)[0]
+                peaks_in_note = [
+                    peak for peak in peaks if peak >= start and peak <= i
+                ]  # Peaks in the current note
+                if len(peaks_in_note) > 1:
+                    vibrato_rate_period = np.mean(
+                        np.diff(peaks_in_note)
+                    )  # Avg period between peaks (in samples)
+                    vibrato_rate = 1 / (
+                        vibrato_rate_period / self.pitch_sr
+                    )  # Frequency of peaks
+                    if vibrato_rate > self.max_vibrato_rate:
+                        end_note = True
+
+                if end_note:
                     end = i
                     ranges.append((start, end))
+                    pitches = [current_pitch]
                     start = i
-                    pitches = None
                 # If the pitch is close enough, add it to the current note
                 else:
                     pitches.append(current_pitch)
+                    end = i
 
         # Create the notes
         notes = []
@@ -127,7 +168,13 @@ class Esig:
 
         return np.mean(self.pitch[note.start : note.end])
 
-    def plot_pitch(self, axes: plt.Axes = None, include_notes: bool = True):
+    def plot_pitch(
+        self,
+        axes: plt.Axes = None,
+        include_notes: bool = True,
+        xlabel: str = "Time (s)",
+        **kwargs
+    ):
         """Plots the guessed pitch. This won't call plt.show(), allowing plot customization.
 
         Parameters
@@ -137,6 +184,10 @@ class Esig:
             If None, a new figure will be created.
         include_notes : bool, optional
             Whether or not to include the guessed notes in the plot, by default True
+        xlabel : str, optional
+            The label of the x-axis, by default &quot;Time (s)&quot;
+        **kwargs
+            Additional arguments to be passed to matplotlib.pyplot.plot()
         """
 
         # Create a new axes if none is given
@@ -144,21 +195,18 @@ class Esig:
             axes = plt.subplot()
 
         # Plot the pitch
-        axes.plot(self.pitch)
+        time = np.linspace(0, len(self.pitch) / self.pitch_sr, len(self.pitch))
+        axes.plot(time, self.pitch, **kwargs)
 
-        # Label and format the axes
-        ticks = np.arange(0, len(self.pitch), len(self.pitch) / 10)
-        time = self.asig.samples / self.asig.sr
-        time_ticks = np.round(np.arange(0, time, time / len(ticks)), 1)
-        axes.set_xticks(ticks, time_ticks)
-        axes.set_xlabel("Time (s)")
+        # Label the axes
+        axes.set_xlabel(xlabel)
         axes.set_ylabel("Pitch (Hz)")
 
         # Plot the notes with average pitch as line
         if include_notes:
             for note in self.notes:
                 axes.plot(
-                    [note.start, note.end],
+                    [note.start / self.pitch_sr, note.end / self.pitch_sr],
                     [self._average_note_pitch(note), self._average_note_pitch(note)],
                     color="red",
                 )
@@ -173,9 +221,9 @@ class Note:
         Parameters
         ----------
         start : int
-            The starting point of this note, in samples.
+            The starting point of this note (inclusive), in samples.
         end : int
-            The ending point of this note, in samples.
+            The ending point of this note (exclusive), in samples.
         pitch : float
             The guessed pitch of this note, in Hz.
         """
